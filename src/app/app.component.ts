b5dbc2ad71691f17d283537cf5238a03f464440d
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2024-07-31 19:34:09 -0400

    added all card titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Card } from '../models/card';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,31 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  cards: Card[] = [
+    // YELLOW
+    new Card('GRUFF', 'RASPY', '#f9df6d', false, false),
+    new Card('HOARSE', 'RASPY', '#f9df6d', false, false),
+    new Card('HUSKY', 'RASPY', '#f9df6d', false, false),
+    new Card('ROUGH', 'RASPY', '#f9df6d', false, false),
+
+    // GREEN
+    new Card('LITTLE', 'FINGERS', '#a0c35a', false, false),
+    new Card('MIDDLE', 'FINGERS', '#a0c35a', false, false),
+    new Card('POINTER', 'FINGERS', '#a0c35a', false, false),
+    new Card('RING', 'FINGERS', '#a0c35a', false, false),
+
+    // BLUE
+    new Card('AMBER', 'ALE STYLES', '#b0c4ef', false, false),
+    new Card('PORTER', 'ALE STYLES', '#b0c4ef', false, false),
+    new Card('SOUR', 'ALE STYLES', '#b0c4ef', false, false),
+    new Card('STOUT', 'ALE STYLES', '#b0c4ef', false, false),
+
+    // PURPLE
+    new Card('BULLDOG', 'FRENCH ___', '#ba81c5', false, false),
+    new Card('FRY', 'FRENCH ___', '#ba81c5', false, false),
+    new Card('HORN', 'FRENCH ___', '#ba81c5', false, false),
+    new Card('KISS', 'FRENCH ___', '#ba81c5', false, false)
+  ]; 
+
   title = 'nyt-connections';
 }
--- src/models/card.ts
+++ src/models/card.ts
@@ -2,12 +2,14 @@ class Card {
     public title: string;
     public category: string;
     public color: string; 
+    public selected: boolean; 
     public found: boolean; 
 
-    constructor(title: string, category: string, color: string, found: boolean) {
+    constructor(title: string, category: string, color: string, selected: boolean, found: boolean) {
         this.title = title;
         this.category = category;
         this.color = color;
+        this.selected = selected;
         this.found = found;
     }
 }
