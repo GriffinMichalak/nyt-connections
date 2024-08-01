bbd0df3e0866bff4524a007a9ebf5e356fd027d5
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2024-08-01 10:22:06 -0400

    connections complete!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-[x: string]: any;
+
   mistakes = 4; 
   connectionsFound = 0; 
   maxSelections = 4;
